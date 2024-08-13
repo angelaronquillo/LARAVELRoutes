7188cf1d09b305a9a339ed20eeaf3f3bf86938fc
Student <Student@LAB5-PC11.ust.edu.ph>
2024-08-13 18:57:04 +0800

    second commit

---
+++ routes/web.php
@@ -0,0 +1,13 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('home');
+});
+Route::get('/contacts', function () {
+    return view('contacts');
+});
+Route::get('/aboutus', function () {
+    return view('aboutus');
+});
